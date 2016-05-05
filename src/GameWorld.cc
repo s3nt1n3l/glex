10fe9edfdd44821f0af64924399d5fa89dc7d2c0
Jack Baxter (student) <jb846@mowa202L-c4.university.brighton.ac.uk>
2016-05-05 16:03:38 +0100

    Added more cube pattern

--- src/GameWorld.cc
+++ src/GameWorld.cc
@@ -3,14 +3,27 @@
 using namespace std;
 
 GameWorld::GameWorld (ApplicationMode mode) : asset_manager(std::make_shared<GameAssetManager>(mode)) {
-asset_manager->AddAsset(std::make_shared<CubeAsset>(1, 1, 1));
-asset_manager->AddAsset(std::make_shared<CubeAsset>(3, 1, 1));
-asset_manager->AddAsset(std::make_shared<CubeAsset>(-1, 1, 1));
-asset_manager->AddAsset(std::make_shared<CubeAsset>(-3, 1, 1));
-asset_manager->AddAsset(std::make_shared<CubeAsset>(1, -1, 1));
-asset_manager->AddAsset(std::make_shared<CubeAsset>(1, -3, 1));
-asset_manager->AddAsset(std::make_shared<CubeAsset>(1, 1, -1));
-asset_manager->AddAsset(std::make_shared<CubeAsset>(1, 1, -3));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(1, 1, 1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-1, 1, 1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-3, 1, 1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(1, -1, 1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(1, -3, 1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(1, 1, -1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(1, 1, -3));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(1, -3, -3));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-3, 1, -3));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-3, -3, 1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-3, -3, -3));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-3, -1, 1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-3, -1, 1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-1, -1, 1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-1, -3, 1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-3, 1, -1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-1, 1, -3));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-3, -1, -1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-1, -1, -1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-1, 1, -1));
+	asset_manager->AddAsset(std::make_shared<CubeAsset>(-1, -3, -1));
 }
  
  void GameWorld::Draw() {
