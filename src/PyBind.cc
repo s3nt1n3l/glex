e825504c24b928287f1741cba9f25dc5c4fdac2e
Jack Baxter (student) <jb846@mowa202L-c4.university.brighton.ac.uk>
2016-05-09 12:43:46 +0100

    Unfinished Python Binding Classes

---
+++ src/PyBind.cc
@@ -0,0 +1,14 @@
+#include <boost/python.hpp>
+#include "PyBind.h"
+
+
+using namespace boost::python;
+
+PythonBindings::PythonBindings(){
+
+}
+
+BOOST_PYTHON_MODULE(libglex){
+
+
+}
---
+++ src/PyBind.h
@@ -0,0 +1,17 @@
+#include "common.h"
+#include "GameAssetManager.h"
+#include <memory>
+
+#ifndef SRC_PYTHONBIND_H_
+#define SRC_PYTHONBIND_H_
+
+class PythonBind{
+
+public:
+	PythonBind();
+
+private:
+
+};
+
+#endif // SRC_PYTHONBIND_H_
