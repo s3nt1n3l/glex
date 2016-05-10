35717c4e37a76e0f0d7ad88b45fcbb057967b5f6
Jack Baxter (student) <jb846@mowa202L-c3.university.brighton.ac.uk>
2016-05-10 11:26:09 +0100

    Fixed Console Spam

--- src/CubeAsset.cc
+++ src/CubeAsset.cc
@@ -76,7 +76,6 @@ void checkError(std::string file, int line) {
 
 void CubeAsset::Draw(GLuint program_token) {
   if(!glIsProgram(program_token)) {
-    std::cerr << "Drawing Cube with invalid program" << std::endl;
     return;
   }
   GLint validation_ok;
